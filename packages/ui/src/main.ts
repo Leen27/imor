de57086af2a75fba778a0c34299eabc6bfd78678
Leen27 <m13840918032@163.com>
2023-12-14 10:55:21 +0800

    test page

--- packages/ui/src/main.ts
+++ packages/ui/src/main.ts
@@ -1,6 +1,8 @@
 import './normalize.css'
 import './style.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-   <div id="editor">test</div>
-`
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const app = createApp(App)
+app.mount('#app')
